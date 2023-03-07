2607fb04b33333ed9735457b579024e9d120b045
Facundo Arambillet <Facundoarambillet@gmail.com>
2023-03-07 20:42:42 -0300

    Creacion de detail y modificacion de la view para que devuelva las respuestas de cada pregunta

--- curso_Django/projectapp/polls/views.py
+++ curso_Django/projectapp/polls/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Estas en la pagina principal")
+    lates_question_list = Question.objects.all()  #El metodo filter trae todas las coincidencias el metodo get unicamente puede traer 1
+    return render(request, "polls/index.html", {   #Render se usa para poder utilizar las views en los templates
+        "lates_question_list": lates_question_list
+    })
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+    question = get_object_or_404(Question, pk= question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
 
 def results(request, question_id):
     return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
