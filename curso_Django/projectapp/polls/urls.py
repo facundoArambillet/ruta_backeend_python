fae42c5b79f8fe6a89f10a7dc5241ceb5e74d03c
Facundo Arambillet <Facundoarambillet@gmail.com>
2023-03-08 13:27:26 -0300

    Cambio de based views a generic views

--- curso_Django/projectapp/polls/urls.py
+++ curso_Django/projectapp/polls/urls.py
@@ -4,9 +4,18 @@ from . import views  # El . se refiere al paquete(carpeta) que estas parado
 
 app_name = "polls"
 
+#FUNCTIONS BASED VIEWS
+# urlpatterns = [
+#     path("", views.index, name="index"),
+#     path("detail/<int:question_id>", views.detail, name="detail"),
+#     path("results/<int:question_id>", views.results, name="results"),
+#     path("vote/<int:question_id>", views.vote, name="vote"),
+# ]
+
+#GENERIC VIEWS
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("detail/<int:question_id>", views.detail, name="detail"),
-    path("results/<int:question_id>", views.results, name="results"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("detail/<int:pk>", views.DetailView.as_view(), name="detail"),
+    path("results/<int:pk>", views.ResultView.as_view(), name="results"),
     path("vote/<int:question_id>", views.vote, name="vote"),
 ]
