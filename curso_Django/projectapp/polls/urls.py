2efa45058733e9373bfc21df18a7ba0e788513dc
Facundo Arambillet <Facundoarambillet@gmail.com>
2023-03-08 11:15:55 -0300

    Actualizacion a manera mas dinamica de llamar una url

--- curso_Django/projectapp/polls/urls.py
+++ curso_Django/projectapp/polls/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views  # El . se refiere al paquete(carpeta) que estas parado
 
+app_name = "polls"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("detail/<int:question_id>", views.detail, name="detail"),
